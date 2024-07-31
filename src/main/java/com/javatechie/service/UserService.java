1ee01dd44ddefbf86c1a3f1862a7d7d2a655af2a
USUPRMR <shivam.kumar1@volkswagen.co.in>
2024-07-31 11:33:22 +0530

    architecture re-design

---
+++ src/main/java/com/javatechie/service/UserService.java
@@ -0,0 +1,17 @@
+package com.javatechie.service;
+
+import com.javatechie.model.User;
+import com.javatechie.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+}
